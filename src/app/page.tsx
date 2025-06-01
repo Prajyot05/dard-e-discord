10e188efd9871d5a8e07c63c2041d2f6d83544f7
Prajyot Tayde <pmtbmt@gmail.com>
2025-06-01 23:34:08 +0530

    add clerk, prisma, and neondb

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div className="h-full w-full flex items-center justify-center">
-      <Button>Hello World</Button>
-    </div>
-  );
-}
